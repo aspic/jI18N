f5e93dc9d7bcdfa46f9c29c59fc48ca88e047616
aspic <kjetil861@gmail.com>
2012-05-15 21:39:35 +0200

    Added backward compability for writing android files to external storage.

---
+++ src/no/mehl/settense/compability/Wrapper.java
@@ -0,0 +1,18 @@
+package no.mehl.settense.compability;
+
+import java.io.File;
+
+import android.content.Context;
+
+/** A wrapper class for backward compability. This wrapper won't be invoked by Dalvik, hence
+ * the compiler won't know that {@link Context#getExternalFilesDir(String)} does not exist.*/
+public class Wrapper {
+	/**
+	 * Simple wrapper method.
+	 * @param ctx The Android {@link Context}.
+	 * @return A new {@link File} handle.
+	 */
+	public static File getExternalFilesDir(Context ctx) {
+		return ctx.getExternalFilesDir(null);
+	}
+}
